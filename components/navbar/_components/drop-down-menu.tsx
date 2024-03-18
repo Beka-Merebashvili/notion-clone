16cc8cab0f5dabfa64c60cdce6ebfcc1a3779124
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-03-19 00:20:22 +0400

    add accordion from shadcn

--- components/navbar/_components/drop-down-menu.tsx
+++ components/navbar/_components/drop-down-menu.tsx
@@ -1,12 +1,43 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import {
+  BarChart,
+  Building2,
+  Code,
+  Code2Icon,
+  DollarSign,
+  Download,
+  GraduationCap,
+  Headphones,
+  Home,
+  Newspaper,
+  PaintBucket,
+  Settings,
+  Smile,
+  Sparkles,
+  ScreenShare,
+  Users,
+  UserPlus,
+  ArrowRight,
+} from "lucide-react";
+
 interface DropDownMenuProps {
-    onClose: () => void;
-  }
-  
-  const DropdownMenu: React.FC<DropDownMenuProps> = ({ onClose }) => {
-    const handleLinkClick = () => {
-      onClose();
-    };
-    return ( <div>this is menu</div> );
+  onClose: () => void;
 }
- 
+
+const DropdownMenu: React.FC<DropDownMenuProps> = ({ onClose }) => {
+  const handleLinkClick = () => {
+    onClose();
+  };
+  return <div>this is menu</div>;
+};
+
 export default DropdownMenu;
