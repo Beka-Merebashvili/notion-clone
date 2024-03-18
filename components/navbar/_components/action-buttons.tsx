0fd07f0c4aeac5e0d91e581ffbee2c2adb8c5237
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-03-18 17:09:22 +0400

    finish navbar

--- components/navbar/_components/action-buttons.tsx
+++ components/navbar/_components/action-buttons.tsx
@@ -5,16 +5,55 @@ import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 import { X, AlignJustify } from "lucide-react";
-import Link from "next/link"; 
+import Link from "next/link";
 
 const ActionButtons = () => {
-    return(
-        <div className="pr-2">
-            <div className="items-center justify-center flex">
-
-            </div>
+  return (
+    <div className="pr-2">
+      <div className=" items-center justify-center flex ">
+        <div className="flex xl:space-x-4">
+          <Link
+            href={"/contact-sales"}
+            className="
+            lg:flex
+            items-center
+            hidden"
+          >
+            <div className="">Request a demo</div>
+          </Link>
+          <div
+            className="font-thin     
+        lg:flex
+            items-center
+            hidden"
+          >
+            |
+          </div>
         </div>
-    )
-}
 
-export default ActionButtons
+        <div className="flex lg:space-x-4 items-center pr-4">
+          <Link href={"/free"}>
+            <Button
+              variant={"outline"}
+              className="
+            lg:flex
+            items-center
+            hidden
+                border-none 
+                text-md
+                
+                "
+            >
+              Log in
+            </Button>
+          </Link>
+          <Link href={"/contact"}>
+            <Button className="hidden lg:block">Get Bird free</Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActionButtons;
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,10 +3,17 @@ import Logo from "./_components/logo";
 import { Menu } from "./_components/menu";
 
 const Navbar = () => {
+
+  const navbarClasses = `
+  flex items-center justify-between space-x-10 bg-white  h-14
+  sticky top-0 z-50 border-b border-gray-200
+`;
   return (
-    <div className="flex items-center">
-      <Logo />
-      <Menu />
+    <div className={navbarClasses}>
+      <div className="flex items-center justify-center">
+        <Logo />
+        <Menu />
+      </div>
       <ActionButtons />
     </div>
   );
