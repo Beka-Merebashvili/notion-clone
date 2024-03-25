52723bba1e18b5db9a10d14b9188225f368069d9
Beka Merebashvili <beqamerebashvili26@gmail.com>
2024-03-25 20:53:48 +0400

    add second section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar/navbar";
 import HeroSection from "./hero-section";
+import SecondSection from "./second-section";
 
 const LeangingPage = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <SecondSection />
     </div>
   );
 };
---
+++ app/(landing-page)/second-section.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import React from 'react';
+import { PiArrowRight } from 'react-icons/pi';
+
+const SecondSection = () => {
+    return (
+      <div>
+        2
+      </div>
+    );
+  }
+  
+  export default SecondSection;
