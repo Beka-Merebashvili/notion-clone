9518676f230ba1e67a2f4bc04d34e787353bed8a
Beka Merebashvili <beqamerebashvili26@gmail.com>
2024-04-03 20:42:53 +0400

    import input , toast , textarea from shadnc

--- app/contact-sales/contact-form.tsx
+++ app/contact-sales/contact-form.tsx
@@ -8,7 +8,40 @@ import { useForm } from "react-hook-form";
 
 import * as z from "zod";
 
+import { Computer, ScreenShare, Share, Smile } from "lucide-react";
 
+import Image from "next/image";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { Button } from "@/components/ui/button";
+
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form";
+
+
+  import { Input } from "@/components/ui/input";
+
+import { useToast } from "@/components/ui/use-toast";
+
+import { Textarea } from "@/components/ui/textarea";
+import { PiCheckLight } from "react-icons/pi";
 
 export default function ContactForm() {
     return (
