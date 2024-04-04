865baa4cf29f3b6297db6255fc9c2fd8bcc18c29
Beka Merebashvili <beqamerebashvili26@gmail.com>
2024-04-04 19:53:02 +0400

    add form schema

--- app/contact-sales/contact-form.tsx
+++ app/contact-sales/contact-form.tsx
@@ -43,6 +43,47 @@ import { useToast } from "@/components/ui/use-toast";
 import { Textarea } from "@/components/ui/textarea";
 import { PiCheckLight } from "react-icons/pi";
 
+const FormSchema = z.object({
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string().email(),
+  job_title: z.string(),
+  company_name: z.string(),
+  help: z.enum([
+    "Evaluate Bird for my company",
+    "Learn More",
+    "Get a Quote",
+    "How to use Bird",
+    "Other",
+  ]),
+  company_size: z.enum([
+    "1-10",
+    "11-50",
+    "51-200",
+    "201-500",
+    "501-1000",
+    "1000+",
+  ]),
+  info: z.string(),
+});
+
+type FormValues = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  job_title: string;
+  company_name: string;
+  help:
+    | "Evaluate Bird for my company"
+    | "Learn More"
+    | "Get a Quote"
+    | "How to use Bird"
+    | "Other";
+  company_size: "1-10" | "11-50" | "51-200" | "201-500" | "501-1000" | "1000+";
+  info: string;
+  terms: boolean;
+};
+
 export default function ContactForm() {
     return (
         <div>HIII</div>
